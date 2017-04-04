c0b3075d955ba1f09449bcaccd0a5f2320b46122
Josh Justice <jjustice@bignerdranch.com>
2017-04-04 14:15:47 -0400

    Remove initial before block

--- spec/direct_spec.rb
+++ spec/direct_spec.rb
@@ -5,10 +5,6 @@ RSpec.describe FollowUser do
   let(:new_followee) { User.new }
   let(:blocking_followee) { User.new }
 
-  before do
-    blocking_followee.blocked << follower
-  end
-
   context 'not already following' do
     it 'lets the follower see the new followee' do
       FollowUser.new(follower: follower, followee: new_followee).call
@@ -23,11 +19,13 @@ RSpec.describe FollowUser do
 
   context 'blocked' do
     it 'does not let the follower see the new followee' do
+      blocking_followee.blocked << follower
       FollowUser.new(follower: follower, followee: blocking_followee).call
       expect(follower.followees).not_to include(blocking_followee)
     end
 
     it 'does not let the followee see the new follower' do
+      blocking_followee.blocked << follower
       FollowUser.new(follower: follower, followee: blocking_followee).call
       expect(blocking_followee.followers).not_to include(follower)
     end
