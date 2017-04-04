aa9cad99d9ec449baa1a7e4a98ba02d0d4ccddbb
Josh Justice <jjustice@bignerdranch.com>
2017-04-04 14:16:44 -0400

    Remove all lets

--- spec/direct_spec.rb
+++ spec/direct_spec.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 RSpec.describe FollowUser do
 
-  let(:follower) { User.new }
-  let(:new_followee) { User.new }
-  let(:blocking_followee) { User.new }
-
   context 'not already following' do
     it 'lets the follower see the new followee' do
+      follower = User.new
+      new_followee = User.new
+
       FollowUser.new(follower: follower, followee: new_followee).call
       expect(follower.followees).to include(new_followee)
     end
 
     it 'lets the followee see the new follower' do
+      follower = User.new
+      new_followee = User.new
+
       FollowUser.new(follower: follower, followee: new_followee).call
       expect(new_followee.followers).to include(follower)
     end
@@ -19,13 +21,19 @@ RSpec.describe FollowUser do
 
   context 'blocked' do
     it 'does not let the follower see the new followee' do
+      follower = User.new
+      blocking_followee = User.new
       blocking_followee.blocked << follower
+
       FollowUser.new(follower: follower, followee: blocking_followee).call
       expect(follower.followees).not_to include(blocking_followee)
     end
 
     it 'does not let the followee see the new follower' do
+      follower = User.new
+      blocking_followee = User.new
       blocking_followee.blocked << follower
+
       FollowUser.new(follower: follower, followee: blocking_followee).call
       expect(blocking_followee.followers).not_to include(follower)
     end
